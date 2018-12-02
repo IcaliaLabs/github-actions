03fe733f1018e76fe9a15d08e99877df7becf94a
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-12-02 16:14:38 -0600

    Fix

--- deploy-stack-to-docker-for-aws/entrypoint.sh
+++ deploy-stack-to-docker-for-aws/entrypoint.sh
@@ -1,38 +1,36 @@
 #! /bin/sh
 
+# Exit immediately if a command exits with a non-zero status:
+set -e
+
 : ${STACK_NAME:=$1}
 : ${STACK_FILE:=$2}
 
 DEPLOY_OPTIONS=""
 
 write_pkey_to_file() {
-  echo ${DOCKER_FOR_AWS_SSH_KEY} > /tmp/ssh-key.pem
+  echo "- Writing SSH private key..."
+  echo "${DOCKER_FOR_AWS_SSH_KEY}" > /tmp/ssh-key.pem
   chmod 0600 /tmp/ssh-key.pem
-  echo "- Wrote SSH private key:"
-  echo ${DOCKER_FOR_AWS_SSH_KEY}
-}
-
-test_ssh_connection() {
-  echo "- Testing the SSH Connection: Listing swarm nodes..."
-  ssh -i /tmp/ssh-key.pem "docker@${SWARM_MANAGER_PUBLIC_DNS}" docker node ls
 }
 
 create_tunnel_to_manager() {
-  ssh -i /tmp/ssh-key.pem -NL localhost:2374:/var/run/docker.sock "docker@${SWARM_MANAGER_PUBLIC_DNS}" &
-  echo "- Opened tunnel to Swarm manager's Docker socket at '${SWARM_MANAGER_PUBLIC_DNS}' to 'localhost:2374'"
+  echo "- Opening SSH tunnel to Swarm manager's Docker socket at '${SWARM_MANAGER_PUBLIC_DNS}'..."
+  ssh -i /tmp/ssh-key.pem -o StrictHostKeyChecking=no -NL localhost:2374:/var/run/docker.sock "docker@${SWARM_MANAGER_PUBLIC_DNS}" &
+  sleep 1s
 }
 
-set_docker_host_to_manager() {
-  export DOCKER_HOST="localhost:2374"
-  echo "- Set 'DOCKER_HOST' env var to 'localhost:2374'"
+test_docker_swarm_connection() {
+  ls -lah /var/run/
+  echo "- Getting remote swarm info..."
+  docker -H localhost:2374 info
 }
 
 connect_to_swarm() {
   echo "Connecting to Swarm:"
   write_pkey_to_file
-  test_ssh_connection
   create_tunnel_to_manager
-  set_docker_host_to_manager
+  test_docker_swarm_connection
 }
 
 set_deploy_options() {
@@ -57,6 +55,5 @@ set_prune_option() {
 
 connect_to_swarm
 set_deploy_options
-echo "DOCKER_HOST=${DOCKER_HOST}"
-echo "DOCKER = `which docker`"
+echo "Running: 'docker stack deploy ${DEPLOY_OPTIONS} ${STACK_NAME}'"
 docker stack deploy ${DEPLOY_OPTIONS} ${STACK_NAME}
