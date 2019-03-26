82ee7f83fa4cb4aa9e8260a24ddab54dadf54759
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-03-26 13:47:35 -0600

    apply

--- gke-deploy/entrypoint.sh
+++ gke-deploy/entrypoint.sh
@@ -4,11 +4,13 @@
 set -e
 
 
+JSON_KEY=${JSON_KEY:-$PLUGIN_JSON_KEY}
 GCLOUD_ZONE=${GCLOUD_ZONE:-$PLUGIN_ZONE}
 GCLOUD_PROJECT=${GCLOUD_PROJECT:-$PLUGIN_PROJECT}
 GCLOUD_CLUSTER=${GCLOUD_CLUSTER:-$PLUGIN_CLUSTER}
-DEPLOY_IMAGE=${DEPLOY_IMAGE:-$PLUGIN_DEPLOY_IMAGE}
-JSON_KEY=${JSON_KEY:-$PLUGIN_JSON_KEY}
+DEPLOY_TEMPLATE=${DEPLOY_TEMPLATE:-$PLUGIN_DEPLOY_TEMPLATE}
+
+DEPLOY_IMAGE_TAG=${COMMIT_SHA:-$DRONE_COMMIT_SHA}
 
 write_key_to_file() {
   if [ -z "${JSON_KEY}" ]
@@ -32,11 +34,29 @@ configure_kubectl() {
     --project $GCLOUD_PROJECT
 }
 
+evaluate_template() {
+  if [ -z "${DEPLOY_IMAGE_TAG}" ]; then DEPLOY_IMAGE_TAG=latest; fi
+
+  if [ ! -f "${DEPLOY_TEMPLATE}" ]
+  then
+    echo "Error: Looks like there's no deploy template!"
+    exit 1
+  fi
+
+  sed "s+{{image-tag}}+${DEPLOY_IMAGE_TAG}+g" $DEPLOY_TEMPLATE > /tmp/deploy.yml
+}
+
+apply_and_wait_for_rollout() {
+  evaluate_template
+  kubectl apply -f /tmp/deploy.yml
+  kubectl rollout status deployment/${GCLOUD_PROJECT}
+}
+
 # Step 1:
 activate_gcloud_service_account
 
-# # Step 2:
+# Step 2:
 configure_kubectl
 
-# # Step 3:
-kubectl get pods
+# Step 3:
+apply_and_wait_for_rollout
